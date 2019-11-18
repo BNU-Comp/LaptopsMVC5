7b40edeb13e381ea829233b1e26f8427617cd47d
21810292 <21810292@HSPD14082887>
2019-11-18 11:40:48 +0000

    Add project files.

---
+++ BNU_LaptopsMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BNU_LaptopsMVC5.MvcApplication" Language="C#" %>
---
+++ BNU_LaptopsMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BNU_LaptopsMVC5.Startup))]
+namespace BNU_LaptopsMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
